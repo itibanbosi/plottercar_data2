00be0903f7e41dea8d410919a15d1668f09dbf83
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-10-11 10:54:17 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,22 @@
-let wait = 0;
+let wait=0;
 enum pen_onoff {
   上げる,
   下げる,
 }
 
+
+
 //% color="#3943c6" block="ﾌﾟﾛｯﾀｰ・ｶｰ" icon="\uf1b9"
 
 namespace eureka_plotter_car {
+
   //% color="#3943c6" weight=71　blockId=plottercar_forward
   //% block="前へ |%F_cm| ｃｍ進む" group="3　基本の動き"
-
+  //% F_cm.min=0 F_cm.max=20
   export function plottercar_forward(F_cm: number): void {
-    led.enable(false);
-    for (let index = 0; index < (F_cm / 18.2) * 512; index++) {
-      let i = 0;
-
-      for (i = 0; i < 1000; i++);
-      {
-      }
+	    led.enable(false)
+    for (let index = 0; index < (F_cm / 18.23) * 512; index++) {
+     let i=0;
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P13, 0);
       pins.digitalWritePin(DigitalPin.P4, 0);
@@ -26,10 +25,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P7, 1);
       pins.digitalWritePin(DigitalPin.P16, 1);
-      for (i = 0; i < 1000; i++);
-      {
-      }
-
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P13, 0);
       pins.digitalWritePin(DigitalPin.P4, 0);
@@ -38,9 +34,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 1);
       pins.digitalWritePin(DigitalPin.P7, 0);
       pins.digitalWritePin(DigitalPin.P16, 1);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P13, 0);
       pins.digitalWritePin(DigitalPin.P4, 1);
@@ -49,9 +43,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 1);
       pins.digitalWritePin(DigitalPin.P7, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P13, 1);
       pins.digitalWritePin(DigitalPin.P4, 1);
@@ -60,9 +52,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 1);
       pins.digitalWritePin(DigitalPin.P7, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P13, 0);
       pins.digitalWritePin(DigitalPin.P4, 1);
@@ -71,9 +61,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P7, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P13, 1);
       pins.digitalWritePin(DigitalPin.P4, 0);
@@ -82,9 +70,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P7, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+       for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P13, 1);
       pins.digitalWritePin(DigitalPin.P4, 0);
@@ -93,9 +79,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P7, 1);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P13, 1);
       pins.digitalWritePin(DigitalPin.P4, 0);
@@ -104,18 +88,18 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P7, 1);
       pins.digitalWritePin(DigitalPin.P16, 1);
-      for (i = 0; i < 1000; i++);
-      {
+       for ( i = 0; i < 1000; i++ );{}
       }
-    }
   }
 
   //% color="#3943c6" weight=69　blockId=plottercar_back
   //% block="後へ |%F_cm| ｃｍ進む" group="3　基本の動き"
-
+  //% F_cm.min=0 F_cm.max=20
   export function plottercar_back(F_cm: number): void {
-    led.enable(false);
-    for (let index = 0; index < (F_cm / 18.2) * 512; index++) {
+	    led.enable(false)
+    for (let index = 0; index < (F_cm / 18.23) * 512; index++) {
+
+      let i=0;
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P13, 1);
       pins.digitalWritePin(DigitalPin.P4, 0);
@@ -124,10 +108,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P7, 1);
       pins.digitalWritePin(DigitalPin.P16, 1);
-      let i = 0;
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P13, 1);
       pins.digitalWritePin(DigitalPin.P4, 0);
@@ -136,20 +117,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P7, 1);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
-      pins.digitalWritePin(DigitalPin.P3, 0);
-      pins.digitalWritePin(DigitalPin.P13, 1);
-      pins.digitalWritePin(DigitalPin.P4, 0);
-      pins.digitalWritePin(DigitalPin.P14, 1);
-      pins.digitalWritePin(DigitalPin.P6, 1);
-      pins.digitalWritePin(DigitalPin.P15, 0);
-      pins.digitalWritePin(DigitalPin.P7, 0);
-      pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P13, 0);
       pins.digitalWritePin(DigitalPin.P4, 1);
@@ -158,9 +126,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P7, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P13, 1);
       pins.digitalWritePin(DigitalPin.P4, 1);
@@ -169,9 +135,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 1);
       pins.digitalWritePin(DigitalPin.P7, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P13, 0);
       pins.digitalWritePin(DigitalPin.P4, 1);
@@ -180,9 +144,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 1);
       pins.digitalWritePin(DigitalPin.P7, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P13, 0);
       pins.digitalWritePin(DigitalPin.P4, 0);
@@ -191,9 +153,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 1);
       pins.digitalWritePin(DigitalPin.P7, 0);
       pins.digitalWritePin(DigitalPin.P16, 1);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P13, 0);
       pins.digitalWritePin(DigitalPin.P4, 0);
@@ -202,33 +162,26 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P7, 1);
       pins.digitalWritePin(DigitalPin.P16, 1);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
     }
   }
 
   //% color="#3943c6" weight=66　blockId=plottercar_L_cycle
   //% block="左回り　角度 |%L_degree| " group="3　基本の動き"
+  //% L_degree.min=0 L_degree.max=360
+    export function plottercar_L_cycle(L_degree: number): void {
+    for (let index = 0; index < (L_degree / 360) * 512 * 1.64; index++) {
+      let i=0;
+      pins.digitalWritePin(DigitalPin.P3, 1);
+      pins.digitalWritePin(DigitalPin.P4, 0);
+      pins.digitalWritePin(DigitalPin.P6, 0);
+      pins.digitalWritePin(DigitalPin.P7, 1);
+      pins.digitalWritePin(DigitalPin.P13, 1);
+      pins.digitalWritePin(DigitalPin.P14, 0);
+      pins.digitalWritePin(DigitalPin.P15, 0);
+      pins.digitalWritePin(DigitalPin.P16, 1);
 
-  export function plottercar_L_cycle(L_degree: number): void {
-    for (let index = 0; index < (L_degree / 360) * 512 * 1.6; index++) {
-      let i = 0;
-
-      for (i = 0; i < 1000; i++);
-      {
-      }
-      pins.digitalWritePin(DigitalPin.P3, 1);
-      pins.digitalWritePin(DigitalPin.P4, 0);
-      pins.digitalWritePin(DigitalPin.P6, 0);
-      pins.digitalWritePin(DigitalPin.P7, 1);
-      pins.digitalWritePin(DigitalPin.P13, 1);
-      pins.digitalWritePin(DigitalPin.P14, 0);
-      pins.digitalWritePin(DigitalPin.P15, 0);
-      pins.digitalWritePin(DigitalPin.P16, 1);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P4, 0);
       pins.digitalWritePin(DigitalPin.P6, 0);
@@ -237,9 +190,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 0);
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P4, 1);
       pins.digitalWritePin(DigitalPin.P6, 0);
@@ -248,9 +199,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 1);
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P4, 1);
       pins.digitalWritePin(DigitalPin.P6, 0);
@@ -259,9 +208,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 1);
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P4, 1);
       pins.digitalWritePin(DigitalPin.P6, 1);
@@ -270,9 +217,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 1);
       pins.digitalWritePin(DigitalPin.P15, 1);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P4, 0);
       pins.digitalWritePin(DigitalPin.P6, 1);
@@ -281,9 +226,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 0);
       pins.digitalWritePin(DigitalPin.P15, 1);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P4, 0);
       pins.digitalWritePin(DigitalPin.P6, 1);
@@ -292,9 +235,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 0);
       pins.digitalWritePin(DigitalPin.P15, 1);
       pins.digitalWritePin(DigitalPin.P16, 1);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P4, 0);
       pins.digitalWritePin(DigitalPin.P6, 0);
@@ -303,10 +244,8 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 0);
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P16, 1);
-      for (i = 0; i < 1000; i++);
-      {
-      }
-    }
+      for ( i = 0; i < 1000; i++ );{}
+     }
   }
 
   //% color="#3943c6" weight=60　blockId=plottercar_rest
@@ -324,9 +263,10 @@ namespace eureka_plotter_car {
 
   //% color="#3943c6" weight=64　blockId=plottercar_R_cycle
   //% block="右回り　角度 |%R_degree| " group="3　基本の動き"
-
+  //% R_degree.min=0 R_degree.max=360
   export function plottercar_R_cycle(R_degree: number): void {
-    for (let index = 0; index < (R_degree / 360) * 512 * 1.6; index++) {
+    for (let index = 0; index < (R_degree / 360) * 512 * 1.64; index++) {
+      let i=0;
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P4, 0);
       pins.digitalWritePin(DigitalPin.P6, 0);
@@ -335,10 +275,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 0);
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P16, 1);
-      let i = 0;
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P4, 0);
       pins.digitalWritePin(DigitalPin.P6, 1);
@@ -347,9 +284,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 0);
       pins.digitalWritePin(DigitalPin.P15, 1);
       pins.digitalWritePin(DigitalPin.P16, 1);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P4, 0);
       pins.digitalWritePin(DigitalPin.P6, 1);
@@ -358,9 +293,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 0);
       pins.digitalWritePin(DigitalPin.P15, 1);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P4, 1);
       pins.digitalWritePin(DigitalPin.P6, 1);
@@ -369,9 +302,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 1);
       pins.digitalWritePin(DigitalPin.P15, 1);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P4, 1);
       pins.digitalWritePin(DigitalPin.P6, 0);
@@ -380,9 +311,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 1);
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+     for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P4, 1);
       pins.digitalWritePin(DigitalPin.P6, 0);
@@ -391,9 +320,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 1);
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P4, 0);
       pins.digitalWritePin(DigitalPin.P6, 0);
@@ -402,9 +329,7 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 0);
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P16, 0);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
       pins.digitalWritePin(DigitalPin.P3, 1);
       pins.digitalWritePin(DigitalPin.P4, 0);
       pins.digitalWritePin(DigitalPin.P6, 0);
@@ -413,21 +338,20 @@ namespace eureka_plotter_car {
       pins.digitalWritePin(DigitalPin.P14, 0);
       pins.digitalWritePin(DigitalPin.P15, 0);
       pins.digitalWritePin(DigitalPin.P16, 1);
-      for (i = 0; i < 1000; i++);
-      {
-      }
+      for ( i = 0; i < 1000; i++ );{}
     }
   }
 
   //% color="#525252" weight=90 blockId=eureka_relay block="ペン |%mode| " group="4_ペンの状態"
   export function plottercar_pen(mode: pen_onoff) {
-    if (mode == pen_onoff.下げる) {
-      pins.servoWritePin(AnalogPin.P8, 0);
-      basic.pause(1000);
+        if (mode == pen_onoff.下げる) {
+     　　pins.servoWritePin(AnalogPin.P8, 0);
+         basic.pause(1000);
+        }
+        if (mode == pen_onoff.上げる) {
+     　　pins.servoWritePin(AnalogPin.P8, 90);
+         basic.pause(100);
+        }
+
     }
-    if (mode == pen_onoff.上げる) {
-      pins.servoWritePin(AnalogPin.P8, 90);
-      basic.pause(100);
-    }
-  }
 }
