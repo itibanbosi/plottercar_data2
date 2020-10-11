6c3f1691c5536f3acdd70234f19a1b9110dd4d76
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-10-11 11:09:57 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -23,7 +23,7 @@ namespace eureka_plotter_car {
   //% F_cm.min=0 F_cm.max=20
   export function plottercar_forward(F_cm: number): void {
 	    led.enable(false)
-    for (let index = 0; index < (F_cm / 18) * 512; index++) {
+    for (let index = 0; index < (F_cm / 17.5) * 512; index++) {
       pins.digitalWritePin(DigitalPin.P3, 0);
       pins.digitalWritePin(DigitalPin.P13, 1);
       pins.digitalWritePin(DigitalPin.P4, 0);
